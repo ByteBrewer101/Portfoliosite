222b2e9484f9e3c5e8b72b6438293151f171507c
ByteBrewer101 <atharvachopde31@gmail.com>
2024-09-21 11:43:02 +0530

    fixed cards and increased dock size on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,35 +9,38 @@ import { dockItems} from "@/lib/data/data";
 
 export default function Home() {
 
-
-
  const ProjectData = [
-  {
-    Title: "Gryffincode: Coding Platform",
-    link: "https://gryffincode.netlify.app/",
-    desc: "Developed a Harry Potter-themed coding platform to help beginners learn programming. Backend built with Express and MongoDB, frontend with React and Tailwind CSS.",
-  },
-  {
-    Title: "Chatterbox: WebSocket Application",
-    link: "https://friendly-beijinho-a03091.netlify.app/",
-    desc: "Created a WebSocket-based chat app using Node.js and React. Backend in TypeScript with real-time communication, frontend with React and Tailwind CSS.",
-  },
-  {
-    Title: "LinkedIn Job Scraper",
-    link: "https://github.com/AtharvaChopde/webscraper",
-    desc: "Developed a web scraper in Python to extract job postings from LinkedIn. Automated data storage in Google Sheets using gspread and Google Colab.",
-  },
-  {
-    Title: "File Sharing Application",
-    link: "https://github.com/AtharvaChopde/python-Notes-sharing-system",
-    desc: "Built a Python-based file-sharing app using Socket and Tkinter for seamless local network file transfers. Integrated PyQRCode for QR code-based sharing.",
-  },
-  {
-    Title: "Unity Game Development Project",
-    link: "https://play.unity.com/en/games/615edb3b-0649-4ffb-a126-90c79880ce77/a-backrooms-horror-based-game",
-    desc: "Developed a game in Unity using C# with AI navigation and raycasting techniques for dynamic in-game character behavior. Optimized game performance for multiple devices.",
-  },
-];
+   {
+     Title: "Gryffincode: ",
+     Title2: "Coding Platform",
+     link: "https://gryffincode.netlify.app/",
+     desc: "Created a Harry Potter-themed platform to help beginners learn coding. Backend with Express and MongoDB, frontend built using React and Tailwind CSS.",
+   },
+   {
+     Title: "Chatterbox:",
+     Title2: "Group chat Application",
+     link: "https://friendly-beijinho-a03091.netlify.app/",
+     desc: "Built a real-time chat app with Node.js and WebSocket. Backend developed in TypeScript, frontend using React and Tailwind CSS for smooth user experience.",
+   },
+   {
+     Title: "LinkedIn Job Scraper",
+     Title2: "Web Scraping application",
+     link: "https://github.com/AtharvaChopde/webscraper",
+     desc: "Developed a Python web scraper for LinkedIn job postings. Automated storage of the scraped data into Google Sheets using gspread and Google Colab.",
+   },
+   {
+     Title: "File Sharing Application",
+     Title2: "Lan based Filesharing",
+     link: "https://github.com/AtharvaChopde/python-Notes-sharing-system",
+     desc: "Created a Python-based file-sharing tool with Tkinter and Socket for local network transfers. QR code generation integrated using PyQRCode for easy sharing.",
+   },
+   {
+     Title: "Unity Game Development Project",
+     Title2: "Game",
+     link: "https://play.unity.com/en/games/615edb3b-0649-4ffb-a126-90c79880ce77/a-backrooms-horror-based-game",
+     desc: "Developed a horror game in Unity using C# with AI navigation and raycasting. Optimized game performance for smooth play on multiple platforms and devices.",
+   },
+ ];
 
   return (
     <HeroHighlight className="min-h-screen flex items-center justify-center flex-col overflow-auto ">
@@ -49,13 +52,14 @@ export default function Home() {
 
       <div
         id="projects"
-        className="grid lg:grid-cols-3 gap-4 w-3/5 snap-start grid-cols-1    "
+        className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 w-full p-20  xl:w-4/5 snap-start grid-cols-1    "
       >
         {ProjectData.map((project) => (
           <CardWithData
             key={project.Title}
             link={project.link}
             title={project.Title}
+            title2={project.Title2}
             desc={project.desc}
           />
         ))}
@@ -63,7 +67,8 @@ export default function Home() {
       <FloatingDock
         items={dockItems}
         desktopClassName="fixed left-1/2 bottom-10 z-100 transform -translate-x-1/2 p-4 rounded-xl shadow-gray-300/50 shadow-xl"
-        mobileClassName="fixed left-10 bottom-10 z-100  "
+        mobileClassName="fixed left-10 bottom-10 z-50 "
+        
       />
 
       <div className="h-40"></div>
--- components/cardWithData.tsx
+++ components/cardWithData.tsx
@@ -6,35 +6,48 @@ import React from "react";
 interface CardWithDataProps {
   link: string;
   title: string;
+  title2:string;
   desc: string;
 }
 
-const CardWithData: React.FC<CardWithDataProps> = ({ link, title, desc }) => {
+const CardWithData: React.FC<CardWithDataProps> = ({ link, title,title2, desc }) => {
 
   const filerouter = useRouter()
 
   return (
-    <div onClick={()=>{filerouter.push("/pages/projectpage")}} className="bg-black relative z-0  flex space-y-4 flex-col w-fit justify-center backdrop-blur-xl p-10 border border-gray-800 rounded-xl h-96 hover:scale-105 hover:bg-blur-xl hover:shadow-white hover:border-none transition ease-in-out duration-200 hover:bg-white hover:text-black">
-      <h1 className="font-bold text-3xl flex justify-start">{title}</h1>
-      <p>{desc}</p>
+    <div
+      onClick={() => {
+        filerouter.push("/pages/projectpage");
+      }}
+      className="bg-black relative z-0  flex space-y-4 flex-col w-auto justify-center backdrop-blur-xl p-10 border border-gray-800 rounded-xl h-96 hover:scale-105 hover:bg-blur-xl hover:shadow-white hover:border-none transition ease-in-out duration-700 hover:bg-white hover:text-black"
+    >
+      <div className=" overflow-y-hidden">
+        <h1 className="font-bold text-3xl flex justify-start">{title}</h1>
+        <h1 className="font-semibold text-xl" >{title2}</h1>
+        <p>
+          {desc.slice(0, 90)}...
+          <a className="text-blue-500 hover:pointer">read more</a>
+        </p>
+      </div>
 
       <button
         onClick={() => {
           window.open(link);
         }}
+        className="  w-fit bg-white rounded-full p-2 transition ease-in-out duration-200 hover:bg-black hover:-rotate-45 "
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6 text-blue-500"
+          stroke="black"
+          className="size-6 hover:stroke-white "
         >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+            d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
           />
         </svg>
       </button>
